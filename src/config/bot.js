9fef5c33573683deed2c068d3cdf25d1025439e8
Spooky-Speaks <spookyspeakss@gmail.com>
2026-05-08 18:47:31 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Discord.gg/Rerir",
+        name: "Playable When | Discord.gg/Rerir",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 4, 
       },
     ],
   },
